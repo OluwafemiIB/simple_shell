a5fff564ed3ff3d8cf52fec0cbc611a8112381be
Chizzyk2 <ziterenna@gmail.com>
2023-11-15 10:20:52 -0800

    SIMPLE_SHELL

---
+++ construct.c
@@ -0,0 +1,101 @@
+#include "shell.h"
+
+/**
+ * _myexit - exits the shell
+ * @info: potential arguments
+ *  Return: Is null
+ *         (0) if info.argv[0] != "exit"
+ */
+int _myexit(info_t *info)
+{
+	int exitcheck;
+
+	if (info->argv[1])
+	{
+		exitcheck = _erratoi(info->argv[1]);
+		if (exitcheck == -1)
+/* An argument checked will be exited */
+		{
+			info->status = 2;
+			print_error(info, "Illegal number: ");
+/* If there is an illegal number error info will be printed */
+			_eputs(info->argv[1]);
+			_eputchar('\n');
+			return (1);
+		}
+		info->err_num = _erratoi(info->argv[1]);
+		return (-2);
+	}
+	info->err_num = -1;
+	return (-2);
+}
+
+/**
+ * _mycd - changes the current directory of the process
+ * @info: potential arguments
+ *  Return: Is 0
+ */
+int _mycd(info_t *info)
+{
+	char *s, *dir, buffer[1024];
+	int chdir_ret;
+
+	s = getcwd(buffer, 1024);
+	if (!s)
+		_puts("TODO: >>getcwd failure emsg here<<\n");
+/* gets the directory of the process */
+	if (!info->argv[1]) /* Checks for arguments */
+	{
+		dir = _getenv(info, "HOME=");
+/* Environment for the new directory */
+		if (!dir)
+			chdir_ret = /* TODO: what this should actually be? */
+				chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
+		else
+			chdir_ret = chdir(dir);
+	}
+	else if (_strcmp(info->argv[1], "-") == 0)
+	{
+		if (!_getenv(info, "OLDPWD="))
+		{
+			_puts(s);
+			_putchar('\n');
+			return (1);
+		}
+		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
+		chdir_ret = /* TODO: what this should actually be */
+			chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
+	}
+	else
+		chdir_ret = chdir(info->argv[1]);
+	if (chdir_ret == -1)
+	{
+		print_error(info, "can't cd to ");
+/* print an error message when cant change directory */
+		_eputs(info->argv[1]), _eputchar('\n');
+	}
+	else
+	{
+		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
+		_setenv(info, "PWD", getcwd(buffer, 1024));
+	}
+	return (0);
+}
+
+/**
+ * _myhelp - changes the current directory of the process
+ * @info: Structure containing potential arguments
+ *  Return: Always 0
+ */
+int _myhelp(info_t *info)
+{
+	char **arg_array;
+
+	arg_array = info->argv;
+	_puts("help call works. Function not yet implemented \n");
+/*Function is called */
+	if (0)
+		_puts(*arg_array);
+	return (0);
+}
+
